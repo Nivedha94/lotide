3fe06dff635a21c55572940766fb82edbe743909
vagrant <nivedha.selvan@gmail.com>
2021-10-27 19:21:26 +0000

    eqArrays correction and assert Equal arrays program

---
+++ assertArraysEqual.js
@@ -0,0 +1,26 @@
+
+const eqArrays = function(array1, array2) {
+  if (array1.length !== array2.length) {
+    return false;
+  } else {
+    for (let i = 0; i < array1.length; i++) {
+      if (array1[i] !== array2[i]) {
+        return false;
+      }
+      
+    }
+    return true;
+  }
+}
+
+const assertArraysEqual = function(arr1, arr2) {
+  if (eqArrays(arr1, arr2)){
+    console.log(`Assertion Passed: ${arr1} === ${arr2}`);
+  } else {
+    console.log(`Assertion Failed: ${arr1} !== ${arr2}`);
+  }
+};
+
+
+
+assertArraysEqual([1,4,3], [1,2,3]);
