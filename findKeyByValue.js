7cfd95b0fcf560d6d617ff1406cd730266689c35
vagrant <nivedha.selvan@gmail.com>
2021-10-28 20:36:07 +0000

    letterPositions

---
+++ findKeyByValue.js
@@ -0,0 +1,29 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`Assertion Failed: ${actual} != ${expected}`);
+  }
+};
+
+const findKeyByValue = function(obj, value) {
+  let keys = Object.keys(obj);
+  console.log(keys);
+  for (let i = 0; i < keys.length; i++){
+    if (obj[keys[i]] = value) {
+      return obj[keys[i]];
+    } 
+    
+  }
+  
+  
+}
+
+const bestTVShowsByGenre = { 
+  sci_fi: "The Expanse",
+  comedy: "Brooklyn Nine-Nine",
+  drama:  "The Wire"
+};
+
+assertEqual(findKeyByValue(bestTVShowsByGenre, "The Wire"), "drama");
+assertEqual(findKeyByValue(bestTVShowsByGenre, "That '70s Show"), undefined);
---
+++ letterPositions.js
@@ -0,0 +1,44 @@
+const letterPositions = function(string) {
+  const results = {};
+  let emptyArray = [];
+  for (let i = 0; i < string.length; i++) {
+    emptyArray.push(string[i]);
+  }
+  for (let j = 0; j < emptyArray.length; j++) {
+    const ch = emptyArray[j];
+    if(!(results[ch])) {
+      results[ch] = [j];
+    } else {
+      results[ch].push(j);
+    }
+  }
+  console.log("++++++++++", results);
+  return results;
+}
+
+
+
+
+const eqArrays = function(array1, array2) {
+  if (array1.length !== array2.length) {
+    return false;
+  } else {
+    for (let i = 0; i < array1.length; i++) {
+      if (array1[i] !== array2[i]) {
+        return false;
+      }
+      
+    }
+    return true;
+  }
+}
+
+const assertArraysEqual = function(arr1, arr2) {
+  if (eqArrays(arr1, arr2)){
+    console.log(`Assertion Passed: ${arr1} === ${arr2}`);
+  } else {
+    console.log(`Assertion Failed: ${arr1} !== ${arr2}`);
+  }
+};
+
+assertArraysEqual(letterPositions("hello")['e'], [1]);
