9c4a8b3c3bf0076eae90cffc90fe828a37e0ebac
vagrant <nivedha.selvan@gmail.com>
2021-10-28 04:55:01 +0000

    countOnly program

---
+++ countLetters.js
@@ -0,0 +1,22 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`Assertion Failed: ${actual} != ${expected}`);
+  }
+};
+
+const countLetters = function(string) {
+  const obj = {};
+  for (let i = 0; i < string.length; i++) {
+    const ch = (string[i]);
+    if (!obj[ch]) {
+      obj[ch] = 0;
+    }
+    obj[ch]++;
+  }
+  console.log(obj);
+};
+
+const words = "Nivedha";
+assertEqual(countLetters(words));
