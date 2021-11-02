0d45e62bc171d98994bffd57df4656e0a5518c59
vagrant <nivedha.selvan@gmail.com>
2021-11-02 20:42:05 +0000

    tail and middle with mocha and chai

--- test/tailTest.js
+++ test/tailTest.js
@@ -1,7 +1,19 @@
-const assertEqual = require('../assertEqual');
+// const assertEqual = require('../assertEqual');
 
-const words = tail(["Hello", "Lighthouse", "Labs"]);
+// const words = tail(["Hello", "Lighthouse", "Labs"]);
 
-assertEqual(words.length, 3); 
-assertEqual(words[0], "Lighthouse");
-assertEqual(words[1], "Labs");
+// assertEqual(words.length, 3); 
+// assertEqual(words[0], "Lighthouse");
+// assertEqual(words[1], "Labs");
+
+const assert = require('chai').assert;
+const tail   = require('../tail');
+
+describe("#head", () => {
+  it("returns 1 for [1, 2, 3]", () => {
+    assert.deepEqual(head([1, 2, 3]), 1);
+  });
+  it("returns '5' for ['5']", () => {
+    assert.deepEqual(head(['5']), '5'); 
+  });
+});
