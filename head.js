1cf8243060bcfe10bd172e5b9796d74ee7e554b9
vagrant <nivedha.selvan@gmail.com>
2021-11-02 19:47:42 +0000

    assertEqual refactor

--- head.js
+++ head.js
@@ -1,10 +1,4 @@
-const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    console.log(`Assertion Passed: ${actual} === ${expected}`);
-  } else {
-    console.log(`Assertion Failed: ${actual} !== ${expected}`);
-  }
-};
+const assertEqual = require('./assertEqual');
 
 const head = function(sampleArray) {
   
@@ -12,5 +6,3 @@ const head = function(sampleArray) {
   
 };
 
-assertEqual(head([5,6,7]), 5);
-assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
