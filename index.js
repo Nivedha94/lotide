4eb0956766572acc0cca6ce05ffa85570d2a4374
vagrant <nivedha.selvan@gmail.com>
2021-11-06 05:05:14 +0000

    exporting all functions

---
+++ index.js
@@ -0,0 +1,26 @@
+const head   = require('./head');
+const tail   = require('./tail');
+const middle = require('./middle');
+const countLetters = require('./countLetters');
+const countOnly = require('./countOnly');
+const findkey = require('/findkey');
+const findKeyByValue = require('/findKeyByValue');
+const letterPositions = require('/letterPositions');
+const map = require('./map');
+const takeUntil = require('./takeUntil');
+const without = require('./without');
+
+
+module.exports = {
+  head:   head,
+  tail:   tail,
+  middle: middle,
+  countLetters: countLetters
+  countOnly: countOnly,
+  findkey: findkey,
+  findKeyByValue: findKeyByValue,
+  letterPositions: letterPositions,
+  map: map,
+  takeUntil: takeUntil,
+  without: without
+};
