b2c648059285b7777df99bd88cb4d77cbed534e8
Nivedha Thamarai Selvan <nivedha.selvan@gmail.com>
2021-11-30 02:05:04 -0600

    Added Mocha functionality to tailTest.js

--- test/tailTest.js
+++ test/tailTest.js
@@ -1,20 +1,20 @@
-const assertEqual = require('../assertEqual');
-const tail   = require('../tail');
-
-const words = tail(["Hello", "Lighthouse", "Labs"]);
-
-assertEqual(words.length, 3); 
-assertEqual(words[0], "Lighthouse");
-assertEqual(words[1], "Labs");
-
-// const assert = require('chai').assert;
+// const assertEqual = require('../assertEqual');
 // const tail   = require('../tail');
 
-// describe("#head", () => {
-//   it("returns 1 for [1, 2, 3]", () => {
-//     assert.deepEqual(head([1, 2, 3]), 1);
-//   });
-//   it("returns '5' for ['5']", () => {
-//     assert.deepEqual(head(['5']), '5'); 
-//   });
-// });
+// const words = tail(["Hello", "Lighthouse", "Labs"]);
+
+// assertEqual(words.length, 3); 
+// assertEqual(words[0], "Lighthouse");
+// assertEqual(words[1], "Labs");
+
+const assert = require('chai').assert;
+const tail   = require('../tail');
+
+describe("#tail", () => {
+  it("returns 3 for [1, 2, 3]", () => {
+    assert.deepEqual(tail([1, 2, 3]), [2, 3]);
+  });
+  it("returns '5' for ['5']", () => {
+    assert.deepEqual(tail(['5']), []); 
+  });
+});
