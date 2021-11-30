c975c168c3e3dbb9753f8fb5fefb5ef434f69762
Nivedha Thamarai Selvan <nivedha.selvan@gmail.com>
2021-11-30 01:29:22 -0600

    assertArrayEqual.js refactored

--- test/assertArrayEqualTest.js
+++ test/assertArrayEqualTest.js
@@ -1,4 +1,5 @@
-const eqArrays = require('./eqArrays');
+const eqArrays = require('../eqArrays');
+const assertArraysEqual = require('../assertArraysEqual')
 
 
 assertArraysEqual([1,4,3], [1,2,3]);
